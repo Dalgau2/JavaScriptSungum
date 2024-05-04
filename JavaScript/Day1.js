6846a745ce95b92e0e0f41d0cc31d5c9bb9eb91f
gautamProject <gmgautam2374@gmail.com>
2024-05-04 21:00:49 +0530

    intro,varibles,operators,output

---
+++ JavaScript/Day1.js
@@ -0,0 +1,67 @@
+// console.log()
+// alert()
+
+// let a= 2
+// let b=2
+// let c=a+b
+
+// console.log( "a+b=",c)
+
+// comment ctrl+/
+
+// Variables Of javaScript
+
+// 3 Type of varible in javaScript
+
+// var,const,let
+
+
+// var
+
+// var a=2
+// var a=4
+// a=1234567
+// console.log(a)
+
+// let
+
+// let a=2
+// a=99999
+// a=3
+// let b=333
+// b=9
+// console.log(a,b)
+
+// const
+
+// const a=10
+// console.log(a)
+
+// *************************Operators
+// Arithmatic Operators
+// +,-,*,/,%,++,
+
+
+// let x = 1;
+// let y=2
+// x**=2
+// console.log(x)
+
+// Comparison Operators
+
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// ?	ternary operator
+
+
+
+var x=2
+var y=5
+
+console.log(x<<=y,"hega ki ni")
